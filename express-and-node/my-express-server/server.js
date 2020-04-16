fd7ef96f9d42b2999c8866be28c78f7c459509ab
MeganPaffrath <meganpaffrath@gmail.com>
2020-04-15 18:54:47 -0700

    repairs and nodemon

--- express-and-node/my-express-server/server.js
+++ express-and-node/my-express-server/server.js
@@ -5,16 +5,28 @@ const app = express();
 
 /*
 get  - request to home/root
-Defines what will happen when someone makes a request for the
-home root directory
+Defines what will happen when someone makes a
+request for the home root directory
 Callback function
   - tells server what to do when request happens
   - request (req)
   - response (resp): tells server how to respond, we must send the response
 
 */
-app.get("/", function(req, resp) {
-  response.send("<h1>HelloWorld</h1>");
+app.get("/", function(req, res) {
+  res.send("<h1>HelloWorld</h1>");
+});
+
+app.get("/contact", function(req, res) {
+  res.send("Contact me at email@email.com");
+});
+
+app.get("/about", function(req, res) {
+  res.send("My name is Megan and I like to code, make music, and care for plants");
+});
+
+app.get("/hobbies", function(req, res) {
+  res.send("<ul><li>Guitar</li><li>Code</li><li>Plants</li></ul>");
 });
 
 // tells it to listen on a specific port
---
+++ resources.md
@@ -0,0 +1,6 @@
+# Resources / Tech
+
+## Servers
+* nodemon.io : a utility that will monitor for changes in your src code and automatically restart your server if it detects changes
+  * `npm install -g nodemon`
+  * `sudo npm install -g nodemon`
