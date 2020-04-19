6cedc76baf2ae1620df761347696074c191e1a9c
MeganPaffrath <meganpaffrath@gmail.com>
2020-04-18 21:08:50 -0700

    failure handling

--- deployment/newsletter-signup/app.js
+++ deployment/newsletter-signup/app.js
@@ -3,8 +3,16 @@
 const express = require("express");
 const urlencodedParser = express.urlencoded({extended: true});
 const app = express();
+const https = require("https");
 
 app.use(express.static("public")); // all static files to use
+
+// mailchimp
+const apiFile = require("../../private-data.json");
+const apiKey = apiFile.apiKeys.mailchimp.api;
+const listID = apiFile.apiKeys.mailchimp.listID;
+const apiKeyLastDigit = apiKey.charAt(apiKey.length - 1);
+// console.log(apiKey + " & " + listID + " & " + apiKeyLastDigit);
 
 
 app.get("/", function(req, res) {
@@ -12,11 +20,52 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", urlencodedParser, function(req, res) {
-  var firstName = req.body.fname;
-  var lastName = req.body.lname;
-  var email = req.body.email;
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
 
-  console.log(firstName + " " + lastName + " " + email);
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data); // turn user data to JSON
+
+  const url = `https://us${apiKeyLastDigit}.api.mailchimp.com/3.0/lists/` +
+  `${listID}`;
+  console.log(url);
+
+  const options = {
+    method: "POST",
+    auth: "meganpaffrath:" + apiKey,
+  };
+
+  const request = https.request(url, options, function(response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
 });
 
 app.listen(4444, function() {
