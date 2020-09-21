893ce3b3e9c3a743aaa66dd14ce3d685948e952c
MeganPaffrath <meganpaffrath@gmail.com>
2020-09-20 20:51:48 -0700

    sql and templating

--- templates/ejs/todolist-v1/app.js
+++ templates/ejs/todolist-v1/app.js
@@ -8,11 +8,18 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 let items = [];
+let workItems =[];
 
 app.post("/", urlencodedParser, function(req, res) {
   item = req.body.item;
-  items.push(item);
-  res.redirect("/");
+  if (req.body.list === "Work") {
+    workItems.push(item)
+    res.redirect("/work")
+  } else {
+    items.push(item);
+    res.redirect("/")
+  }
+  // console.log(req.body);
 });
 
 app.get("/", function(req, res) {
@@ -29,11 +36,25 @@ app.get("/", function(req, res) {
   var day = today.toLocaleDateString("en-US", options);
 
   res.render('list', {
-    kindOfDay: day,
-    listItems: items
+    listTitle: day,
+    newListItems: items
   });
 
 });
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
+
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
+
+app.get("/about", function(req,res) {
+  res.render("about")
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
