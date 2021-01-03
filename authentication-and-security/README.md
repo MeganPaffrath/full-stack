229731cf31812d000a2d703008cc6bdbf076422c
meganpaffrath <meganpaffrath@gmail.com>
2021-01-02 20:21:50 -0800

    basic encryption

---
+++ authentication-and-security/README.md
@@ -0,0 +1,23 @@
+# Authentication and Security
+
+# Useful things
+
+- mongoose-encryption
+  - npm i mongoose-encryption
+  - encryption
+- dotenv
+  - npm i dotenv
+  - NOTE: .env file is hidden
+  - this is wehre you store evironment vars
+
+# Authentication
+
+- Why authenticate?
+  - need to save user data
+  - restrict access to certain areas for unpaid users
+- Levels of security
+  - create account, saving users to db, user data, users, login, logout, OAuth, social logins, sessions, cookies, encryption
+
+# Security
+
+- Database encryption
--- authentication-and-security/secrets/app.js
+++ authentication-and-security/secrets/app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -15,10 +17,13 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
   email: String,
   password: String
-};
+});
+
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
